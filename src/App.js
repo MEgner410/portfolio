3789ad99f8455948660dfa36d877bacec06e0435
MEgner410 <michael.egner410@gmail.com>
2023-12-15 14:09:24 -0800

    git commit -m "update portfolio"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppContainer, Heading, Paragraph } from './styles/StyledComponent.js.';
 
-function App() {
+
+function AppWithStyledComponents() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <Heading>Michael Portfolio</Heading>
+      <Paragraph>Welcome to my Portfolio! This is the start of something great...</Paragraph>
+    </AppContainer>
+  )
 }
 
-export default App;
+function AppWithoutStyledComponents() {
+  return (
+  <div className="App">
+    <h1>Michael's Portfolio</h1>
+  <p>Welcome to my Portfolio! This is the start of something great...</p>
+</div>
+
+);
+
+}
+
+export default AppWithStyledComponents;
